312e9d421a56033928221aef859c03e0eb7b7ee7
Augustinas Markevicius <augustinas.markevicius@gmail.com>
2015-03-23 14:39:56 +0000

    Uncommented one of the feature tests

--- spec/feature/battleship_ship_spec.rb
+++ spec/feature/battleship_ship_spec.rb
@@ -1,9 +1,9 @@
-# require 'ship'
-# require 'capybara/rspec'
+require 'ship'
+require 'capybara/rspec'
 
-# feature 'ships can be created and destroyed' do
-#   scenario 'ships can have different sizes' do
-#     ship = Ship.new 3
-#     expect(ship.size).to eq 3
-#   end
-# end
+feature 'ships can be created and destroyed' do
+  scenario 'ships can have different sizes' do
+    ship = Ship.new 3
+    expect(ship.size).to eq 3
+  end
+end
