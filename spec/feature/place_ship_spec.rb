216f5c5539beb325e752fb263fd2f013a5c39655
Augustinas Markevicius <augustinas.markevicius@gmail.com>
2015-03-24 11:21:12 +0000

    FT to check if the ship is placed on the board, updated README.md

--- spec/feature/place_ship_spec.rb
+++ spec/feature/place_ship_spec.rb
@@ -3,11 +3,16 @@ require 'board'
 require 'ship'
 
 feature 'game preparation: ' do
+  let(:board) { Board.new }
+  let(:ship) { Ship.new }
+
   scenario 'player can place a ship' do
-    board = Board.new
-    ship = Ship.new
     location = 1
     board.place(ship, location)
     expect(board.get(location)).to eq(ship)
+  end
+
+  scenario 'ship cannnot be place outside of the board' do
+    expect { board.place(ship, 0) }.to raise_error 'Invalid Location'
   end
 end
