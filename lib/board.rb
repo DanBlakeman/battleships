831bcd2d7094fe18ddb5f021d7d5a5f16d19a1f7
Ilya Faybisovich <Ilya.Faybisovich@gmail.com>
2015-03-24 16:26:36 +0200

    Player can place a ship using row and column

--- lib/board.rb
+++ lib/board.rb
@@ -1,13 +1,24 @@
 class Board
   def initialize
-    @board = []
+    @board = {
+      :A => Array(0..9),
+      :B => Array(0..9),
+      :C => Array(0..9),
+      :D => Array(0..9),
+      :E => Array(0..9),
+      :F => Array(0..9),
+      :G => Array(0..9),
+      :H => Array(0..9),
+      :I => Array(0..9),
+      :J => Array(0..9)
+    }
   end
 
-  def place(ship, location)
-    @board[location] = ship
+  def place(ship, column, row)
+    @board[column][row] = ship
   end
 
-  def get(location)
-    @board[location]
+  def get(column, row)
+    @board[column][row]
   end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -2,8 +2,8 @@ require 'board'
 
 describe Board do
   it '#get should return ship at location' do
-    location = 1
-    subject.place(:ship, location)
-    expect(subject.get(location)).to eq(:ship)
+    location = :C, 1
+    subject.place(:ship, *location)
+    expect(subject.get(*location)).to eq(:ship)
   end
 end
