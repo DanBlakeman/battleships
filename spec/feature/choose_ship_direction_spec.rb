32a2b927d36fe2c402ead9046caf562f175f5072
Dan Blakeman <DanBlakeman@users.noreply.github.com>
2015-03-24 11:30:12 +0000

    Work in progress on third user story, feature test written

---
+++ spec/feature/choose_ship_direction_spec.rb
@@ -0,0 +1,15 @@
+require 'capybara/rspec'
+require 'ship'
+require 'board'
+
+feature 'create layout of ships' do
+  scenario 'can face ship right' do
+    size = 2
+    ship = Ship.new(size)
+    board = Board.new
+    location = 3
+    direction = 'right'
+    board.place(ship, location, direction)
+    expect(board[location..location + ship.size - 1].count(ship)).to eq size
+  end
+end
